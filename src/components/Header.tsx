186051f66a1be8013432046396cae5415fe5f814
JoseGro10 <edgardo30g@gmail.com>
2025-04-10 10:57:47 -0600

    Creando el formulario de busqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { useMemo } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
 export default function Header() {
 
     const location = useLocation()
+    const isHome = useMemo(() => location.pathname === '/', [location.pathname])
 
     return (
         <header className="bg-slate-800">
@@ -21,6 +23,43 @@ export default function Header() {
                         }>Favoritos</NavLink>
                     </nav>
                 </div>
+
+
+                { isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg 
+                        shadow-lg space-y-6">
+                        <div className='space-y-4'>
+                            <label 
+                                htmlFor="ingredient" 
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o ingredientes</label>
+                            <input 
+                                id="ingredient"
+                                type="text" 
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                placeholder='Nombre o ingrediente' />
+                        </div>
+                        <div className='space-y-4'>
+                            <label 
+                                htmlFor="ingredient" 
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoria</label>
+                            <select 
+                                id="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                >
+                                <option value="">-- Selecciona una categoria --</option>
+                            </select>
+                        </div>
+                        <input 
+                            type="submit" 
+                            value="Buscar recetas"
+                            className="cursor-pointer bg-orange-800 hoover:bg-orange-900 text-white 
+                            font-extrabold w-full p-2 rounded-lg uppercase"
+                        />
+                    </form>
+                )}        
+
             </div>
         </header>
     )
