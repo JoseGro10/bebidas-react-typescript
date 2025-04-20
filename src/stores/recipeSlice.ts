3425d028f2192f101001045a9d0348eb2cfaec14
JoseGro10 <edgardo30g@gmail.com>
2025-04-19 21:34:19 -0600

    Obteniendo las categorias

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,17 @@
 import { StateCreator } from "zustand"
+import { getCategories } from "../services/RecipeService"
 
 type Category = {}
 
 export type RecipesSliceTypes ={
     categories: Category[]
+    fetchCategories: () => Promise<void>
 }
 
 export const createRecipesSlice : StateCreator<RecipesSliceTypes> = () => ({
-    categories: []
+    categories: [],
+    fetchCategories: async () => {
+        console.log("Fetching categories...")
+        getCategories()
+    }
 })
