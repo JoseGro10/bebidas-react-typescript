a3919a2345887ecec246dc5597b9e08a224b57f9
JoseGro10 <edgardo30g@gmail.com>
2025-05-14 17:38:08 -0600

    Primeros pasos mostrando las bebidas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
-import { CategoriesAPIResponse, DrinksAPIResponse, SearchFilterSchema } from '../utils/recipes-schema'   
+import { CategoriesAPIResponse, DrinksAPIResponse, DrinkAPIReponse, SearchFilterSchema } from '../utils/recipes-schema'   
 
 export type Categories = z.infer<typeof CategoriesAPIResponse> 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
+export type Drink = z.infer<typeof DrinkAPIReponse>
