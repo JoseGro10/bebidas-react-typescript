956752c7a9b63edcff2ea3b40e742b4646947a64
JoseGro10 <edgardo30g@gmail.com>
2025-05-15 13:02:39 -0600

    Agregando Headless UI para mostrar las recetas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
-import { CategoriesAPIResponse, DrinksAPIResponse, DrinkAPIReponse, SearchFilterSchema } from '../utils/recipes-schema'   
+import { CategoriesAPIResponse, DrinksAPIResponse, DrinkAPIReponse, SearchFilterSchema, RecipeAPIResponseSchema } from '../utils/recipes-schema'   
 
 export type Categories = z.infer<typeof CategoriesAPIResponse> 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
 export type Drink = z.infer<typeof DrinkAPIReponse>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
