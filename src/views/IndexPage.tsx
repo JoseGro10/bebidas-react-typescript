615059fcdf36e401d14f520ccb5c2c040c78fec0
JoseGro10 <edgardo30g@gmail.com>
2025-05-15 09:25:05 -0600

    Finalizando el codigo para mostrar las bebidas

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -8,18 +8,18 @@ export default function IndexPage() {
   const hasDrinks = useMemo(() => drinks.drinks.length, [drinks] )
 
   return (
-    <>
+    <div className="m-8">
         <h1 className="text-6xl font-extrabold">Recetas</h1>     
 
         { hasDrinks ? (
-          <>
+          <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
             {drinks.drinks.map((drink) => (
               <DrinkCard
                 key={drink.idDrink}
                 drink={drink}
               />
             ))}
-          </>
+          </div>
         ) : (
           <p className="my-10 text-center text-2xl">
             No hay resultados aún, utiliza el formulario para buscar recetas 
@@ -27,6 +27,6 @@ export default function IndexPage() {
         )
 
         }
-    </>
+    </div>
   )
 }
