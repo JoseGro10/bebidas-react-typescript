0d56b5a656377bc4a1c0536764bd606ea1495d88
JoseGro10 <edgardo30g@gmail.com>
2025-05-17 23:05:01 -0600

    Mostrando los ingredientes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,31 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { Fragment } from 'react';
 import { useAppStore } from '../stores/useAppStore';
+import { Recipe } from '../types';
 
 export default function Modal() {
 
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+
+    const renderIngredients = () => {
+        const ingredients : JSX.Element[] = []
+        for(let i=1; i<=15; i++){
+            const ingredient = selectedRecipe[`strIngredient${i}` as keyof Recipe]
+            const measure = selectedRecipe[`strMeasure${i}` as keyof Recipe]
+
+
+            if(ingredient && measure){
+                ingredients.push(
+                    <li key={i} className='text-lg font-normal'>
+                        {ingredient} - {measure}
+                    </li>
+                )
+            }
+        }
+        return ingredients
+    }
 
     return (
         <>
@@ -47,6 +66,7 @@ export default function Modal() {
                                     <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                                         Ingredientes y cantidades
                                     </DialogTitle>
+                                    {renderIngredients()}
                                     <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                                         {selectedRecipe.strInstructions}
                                     </DialogTitle>
