9c80103df25b7cb7246d9c65d4fb4284cef59b5a
JoseGro10 <edgardo30g@gmail.com>
2025-05-18 09:43:51 -0600

    Mostrando de forma condicional el texto agregar

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ export default function Modal() {
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
     const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
-
+    const favoriteExists = useAppStore((state) => state.favoriteExists)
 
     const renderIngredients = () => {
         const ingredients : JSX.Element[] = []
@@ -86,7 +86,7 @@ export default function Modal() {
                                             type='button'
                                             className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
                                             onClick={() => handleClickFavorite(selectedRecipe)}
-                                        >Agregar a favoritos</button>
+                                        >{favoriteExists(selectedRecipe.idDrink) ? 'Eliminar favorito' : 'Agregar a favoritos'}</button>
                                     </div>
                                 </DialogPanel>
                             </TransitionChild>
--- src/stores/favoriteSlice.ts
+++ src/stores/favoriteSlice.ts
@@ -5,12 +5,13 @@ import { Recipe } from "../types"
 export type FavoriteSliceType = {
     favorites: Recipe[],
     handleClickFavorite: (recipe: Recipe) => void,
+    favoriteExists: (id: Recipe['idDrink']) => boolean
 }
 
 export const createFavoritesSlice : StateCreator<FavoriteSliceType> = (set, get) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
-        if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
+        if(get().favoriteExists(recipe.idDrink)){
             set((state) => ({
                 favorites: state.favorites.filter((favorite) => favorite.idDrink !== recipe.idDrink)
             }))
@@ -19,5 +20,8 @@ export const createFavoritesSlice : StateCreator<FavoriteSliceType> = (set, get)
                 favorites: [...state.favorites, recipe]
             }))
         }
+    },
+    favoriteExists: (id) => {
+        return get().favorites.some(favorite => favorite.idDrink === id)
     }
 })
