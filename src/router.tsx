015e2b1ac72c6ea3b32d0ed9ab32017a4f460903
JoseGro10 <edgardo30g@gmail.com>
2025-05-19 11:50:47 -0600

    Perfomance en tus apps con React router Dom y build

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import IndexPage from "./views/IndexPage"
-import FavoritesPages from "./views/FavoritesPages"
 import Layout from "./layouts/Layout"
+import { lazy, Suspense } from "react"
+
+const IndexPage = lazy(() => import("./views/IndexPage"))
+const FavoritesPage = lazy(() => import("./views/FavoritesPages"))
 
 export default function router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index/>
-          <Route path="/favoritos" element={<FavoritesPages />} />
+          <Route path="/" element={
+            <Suspense fallback="Cargando...">
+              <IndexPage />
+            </Suspense> } index/>
+          <Route path="/favoritos" element={
+            <Suspense fallback="Cargando...">
+              <FavoritesPage />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
