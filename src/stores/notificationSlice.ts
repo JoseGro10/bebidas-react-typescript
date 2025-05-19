4037721eb1c89fefce548b68a93d354e3dc583e6
JoseGro10 <edgardo30g@gmail.com>
2025-05-19 11:16:47 -0600

    Cerrando la notificacion

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -10,11 +10,12 @@ type Notification = {
 export type NotificationSliceType = {
     notification: Notification
     showNotification: (payload: Pick<Notification, "text" | "error">) => void
+    hideNotification: () => void
 }
 
 export const createNotificationSlice: StateCreator<NotificationSliceType & FavoriteSliceType, [], [], NotificationSliceType> = (set, get) => ({
     notification: {
-        text: "",
+        text: '',
         error: false,
         show: false
     },
@@ -24,6 +25,18 @@ export const createNotificationSlice: StateCreator<NotificationSliceType & Favor
                 text: payload.text,
                 error: payload.error,
                 show: true
+            }
+        })
+        setTimeout(() => {
+            get().hideNotification()
+        }, 5000)
+    },
+    hideNotification: () => {
+        set({
+            notification: {
+                text: '',
+                error: false,
+                show: false
             }
         })
     }
