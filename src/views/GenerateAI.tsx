6ace46e5ba1949aa8e006ca623b3eef27991d62d
JoseGro10 <edgardo30g@gmail.com>
2025-05-20 17:22:17 -0600

    Añadiendo la validacion al formulario

--- src/views/GenerateAI.tsx
+++ src/views/GenerateAI.tsx
@@ -1,25 +1,42 @@
+import { useAppStore } from "../stores/useAppStore"
 
 export default function GenerateAI() {
-  
-  
+
+  const showNotification = useAppStore(state => state.showNotification)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const form = new FormData
+    const prompt = form.get('prompt') as string
+
+    if(prompt.trim() === '') {
+      showNotification({
+        text: 'La busqueda no puede estar vacia',
+        error: true
+      })
+      return
+    }
+  }
+
   return (
     <>
       <h1 className="text-6xl font-extrabold">Generar Receta con IA</h1>
 
       <div className="max-w-4xl mx-auto">
-        <form  
-          onSubmit={() => {}}
+        <form
+          onSubmit={handleSubmit}
           className='flex flex-col space-y-3 py-10'
         >
           <div className="relative">
-            <input 
-              name="prompt" 
-              id="prompt" 
-              className="border bg-white p-4 rounded-lg w-full border-slate-800" 
+            <input
+              name="prompt"
+              id="prompt"
+              className="border bg-white p-4 rounded-lg w-full border-slate-800"
               placeholder="Genera una receta con ingredientes. Ej. Bebida con Tequila y Fresa"
             />
-            <button 
-              type="submit" 
+            <button
+              type="submit"
               aria-label="Enviar"
               className={`cursor-pointer absolute top-1/2 right-5 transform -translate-x-1/2 -translate-y-1/2`}
             >
@@ -37,6 +54,6 @@ export default function GenerateAI() {
         </div>
       </div>
 
-    </> 
+    </>
   )
 }
