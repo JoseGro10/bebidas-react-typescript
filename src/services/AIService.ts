b23c284bda2f0841b1f7afc2f1a2b1bfc784fda5
JoseGro10 <edgardo30g@gmail.com>
2025-05-21 16:41:50 -0600

    Enviando peticiones hacia la IAy leyendo las respuestas

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -4,7 +4,9 @@ import { openRouter } from "../lib/ai"
 export default {
     async generateRecipe(prompt: string){
         const result = streamText({
-            model: openRouter('')
+            model: openRouter('meta-llama/llama-3.3-70b-instruct:free'),
+            prompt: prompt,
         })
+        return result.textStream
     }
 }
